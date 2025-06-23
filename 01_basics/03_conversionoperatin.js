6c9818a260ed03db1a31a4eecf8904ac8d07bd2c
Sachin87077 <154797940+Sachin87077@users.noreply.github.com>
2025-06-23 05:40:27 +0000

    changes

---
+++ 01_basics/03_conversionoperatin.js
@@ -0,0 +1,11 @@
+let score="33";
+console.log(typeof score );
+let valinnumber=Number(score);
+console.log(typeof valinnumber);
+let isloggedin="sachin";
+let booleanisloggein=Boolean(isloggedin);
+console.log(booleanisloggein);
+//1=>true 0=>false;
+//" "=>false
+//"sachin"=>true;
+
