39438cb252b03afbcd572d9b344850fb137d3a9c
Sachin87077 <154797940+Sachin87077@users.noreply.github.com>
2025-06-23 05:29:29 +0000

    da

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const account_Id=144554;
+let email="sachinpal87077@gmail.com";
+var acountpass="22243";
+
+console.log(account_Id);
+email="saxhinpal7217@gmail.com";
+console.log(email);
+console.table([account_Id,email,acountpass]);
+
+/*
+prefre not to use var beacaue of issue in block scope and functiona scope
+
+
+*/
+
+
+
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict "//treat all js code as newer version 
+//alert(3+3)//we arer usinh nodejs not browser
+let name ="hitesh"
+let age="24"
+//number =>2 to 53;
+//bigint//
+//string =?""
+//null standalone value 
+//symbol uniqenesss 
+//object 
+console.log(typeof  age);
+console.log(typeof undefined);
+
+console.log(typeof null);
+
