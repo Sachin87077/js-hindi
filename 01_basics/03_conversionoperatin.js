770c703980393525c27a8aa904c7d2a7fd66a51f
Sachin87077 <154797940+Sachin87077@users.noreply.github.com>
2025-06-23 06:41:02 +0000

    changes

--- 01_basics/03_conversionoperatin.js
+++ 01_basics/03_conversionoperatin.js
@@ -6,6 +6,14 @@ let isloggedin="sachin";
 let booleanisloggein=Boolean(isloggedin);
 console.log(booleanisloggein);
 //1=>true 0=>false;
-//" "=>false
-//"sachin"=>true;
+let str1="hello"
 
+let str2="sachin"
+let str3=str1+str2;
+console.log(str3);
+console.log("1"+2)
+console.log(1+2)
+console.log(1+"2") 
+let num1,num2,num3
+num1=num2=num3=2*2;
+console.table([num1,num2,num3]);
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,9 @@
+console.log(2>1)//daatayep shoiulsbe same for comparisodn 
+console.log(null>0)
+console.log(null==0)//null isnot converted o 0 in this case 
+
+console.log(null>=0)
+console.log(undefined==0)
+console.log(undefined>0)
+console.log(undefined<0)
+console.log("2">1)
