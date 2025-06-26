5306c014f6b26e4db1af83dd693a910f76f568d5
Sachin87077 <154797940+Sachin87077@users.noreply.github.com>
2025-06-26 04:17:40 +0000

    maths

---
+++ 01_basics/07_numandmaths.js
@@ -0,0 +1,12 @@
+const score =400
+console.log(score)
+const balance=new Number(100)
+
+console.log(balance)
+console.log(balance.toFixed(1));
+const othernumber=23.5555
+console.log(othernumber.toPrecision(3));
+
+console.log(Math)
+console.log(Math.random());
+
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,37 @@
-console.log("sachin")
+const bignum=2343223542n;
+const heros=["shaktiman","juiir ji"];//array
+let myobj={
+    name :"sachin",
+    age :18,
+}//object
+ const functionname =function(){
+    console.log("hello this is sachin");
+
+
+
+
+}//functionn
+
+
+
+//Memoery allocation 
+//Stack(priitive/by copy formation)heap(non primitive/int main value )
+let myname="sachin";
+let anothername=myname;
+anothername =raj
+console.log(myname);
+console.log(anothername);
+let user1= {
+    email :"Agoogle",
+    age:23,
+    salary:2222222
+
+}
+
+let user2=user1;
+user2.email="microsoft@.com"
+console.log(user1.email);
+console.log(user2.email);
+
+
+
